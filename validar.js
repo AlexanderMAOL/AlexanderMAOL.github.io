374f9aacf910b31d070afca227981ea5ad99d1a0
AlexanderMAOL <magana.olivares.hector.alexander@gmail.com>
2024-09-16 15:08:15 -0600

    Update validar.js

--- validar.js
+++ validar.js
@@ -1,6 +1,6 @@
 function validar(form){
     let nombre =form.nombre.value.trim()
- if (nombre == ""){
+ if (nombre == "" || nombre = ["1","2","3","4","5"]){
     alert("Por favor, ingrese su nombre");
     return false
  }   
