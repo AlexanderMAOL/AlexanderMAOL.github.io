1455d857405286606287570505b0c95b54d52c71
AlexanderMAOL <magana.olivares.hector.alexander@gmail.com>
2024-09-19 19:43:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('dniForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evitar que el formulario se envíe
+    event.preventDefault();
 
     var letras = ['T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D', 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L', 'C', 'K', 'E'];
     
@@ -7,28 +7,17 @@ document.getElementById('dniForm').addEventListener('submit', function(event) {
     var userLetter = document.getElementById('letter').value.toUpperCase();
 
     var resultDiv = document.getElementById('result');
-       
-    if (dni === "") {
-        alert("Por favor ingrese un número de DNI");
-        return;
-    }
-    
+
     if (dni < 0 || dni > 99999999) {
         alert("El número de DNI proporcionado no es válido.")
         return;
     }
 
-    
     var calculatedLetter = letras[dni % 23];
 
-    
     if (calculatedLetter !== userLetter) {
         alert ("La letra proporcionada no es correcta. La letra correcta es: " + calculatedLetter)
     } else {
        alert ("El número y la letra del DNI son correctos.")
     }
-    let dni =form.dni.value.trim()
-     if (dni == ""){
-    alert("Por favor, un numero de DNI ");
-    return false
 });
